a01853d095e005eae88b80149baa2a631c23c422
mcclene <p47284513@gmail.com>
2026-09-15 08:40:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+João Victor Guidetti Bartz
+08:38 (há 1 minuto)
+para mim
+
 function somar() {
     let numero1 = Number(document.getElementById("numero1").value);
     let numero2 = Number(document.getElementById("numero2").value);
@@ -15,3 +20,53 @@ function subtrair() {
 
     document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
 }
+
+function multiplicar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 * numero2;
+
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
+
+function dividir() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    if (numero2 === 0) {
+        document.getElementById("resultado").innerHTML = "Não é possível dividir por zero.";
+        return;
+    }
+
+    let resultado = numero1 / numero2;
+
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
+
+function porcentagem() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = (numero1 * numero2) / 100;
+
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado + "%";
+}
+
+function adicionarVirgula() {
+    let input1 = document.getElementById("numero1");
+    let input2 = document.getElementById("numero2");
+
+    if (document.activeElement === input1 && !input1.value.includes(".")) {
+        input1.value += ".";
+    } else if (document.activeElement === input2 && !input2.value.includes(".")) {
+        input2.value += ".";
+    }
+}
+
+function limpar() {
+    document.getElementById("numero1").value = "";
+    document.getElementById("numero2").value = "";
+    document.getElementById("resultado").innerHTML = "Resultado:";
+}
+
